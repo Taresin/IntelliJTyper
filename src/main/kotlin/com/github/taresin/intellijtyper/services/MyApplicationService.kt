5fbc4a897ebecd49a29002fe427ede5c19e111bc
GitHub Action <action@github.com>
2023-01-31 01:39:02 +0000

    Template cleanup

--- src/main/kotlin/com/github/taresin/intellijtyper/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/taresin/intellijtyper/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.taresin.intellijtyper.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.taresin.intellijtyper.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/taresin/intellijtyper/services/MyProjectService.kt
+++ src/main/kotlin/com/github/taresin/intellijtyper/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.taresin.intellijtyper.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.taresin.intellijtyper.MyBundle
 
 class MyProjectService(project: Project) {
 
